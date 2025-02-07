912164bcfb0e368d138f6f23bb11d9ba8f582a28
DESKTOP-00LKSEK\MSI <hhs9200@gmail.com>
2025-02-07 15:13:29 +0900

    BT 작업 내용

---
+++ Source/ReadyOrNot/Private/HHS/NPC_AIController.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HHS/NPC_AIController.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "HHS/NPC.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "ReadyOrNot/FpsCharacter/ReadyOrNotCharacter.h"
+
+ANPC_AIController::ANPC_AIController(FObjectInitializer const& ObjectInitializer)
+{
+//	SetupPerceptionSystem() ;
+}
+
+void ANPC_AIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	if (ANPC* const npc = Cast<ANPC>(InPawn))
+	{
+		if (UBehaviorTree* const tree = npc->GetBehaviorTree())
+		{
+			UBlackboardComponent* b;
+			UseBlackboard(tree->BlackboardAsset, b);
+			Blackboard = b;
+			RunBehaviorTree(tree);
+		}
+	}
+}
+
+
+ void ANPC_AIController::SetupPerceptionSystem()
+{
+	SightConfig = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight Config"));
+	if (SightConfig)
+	{
+		SetPerceptionComponent(*CreateDefaultSubobject<UAIPerceptionComponent>(
+		TEXT("PerceptionComponent")));
+		SightConfig->SightRadius = 500.0f;
+		SightConfig->LoseSightRadius = SightConfig->SightRadius + 25.f;
+		SightConfig->PeripheralVisionAngleDegrees = 90.0f;
+		SightConfig->SetMaxAge(5.f);
+		SightConfig->AutoSuccessRangeFromLastSeenLocation = 520.0f;
+		SightConfig->DetectionByAffiliation.bDetectEnemies = true;
+		SightConfig->DetectionByAffiliation.bDetectFriendlies = true;
+		SightConfig->DetectionByAffiliation.bDetectNeutrals = true;
+
+
+		GetPerceptionComponent()->SetDominantSense(*SightConfig->
+		GetSenseImplementation());
+		GetPerceptionComponent()->OnTargetPerceptionUpdated.AddDynamic(this,
+		&ANPC_AIController::OnTargetDetected);
+		GetPerceptionComponent()->ConfigureSense((*SightConfig));
+	}
+}
+
+
+void ANPC_AIController::OnTargetDetected(AActor* Actor, FAIStimulus const Stimulus)
+{
+	if (auto* const ch = Cast<AReadyOrNotCharacter>(Actor))
+	{
+		GetBlackboardComponent()->SetValueAsBool("CanSeePlayer", Stimulus.WasSuccessfullySensed());
+	}
+}
