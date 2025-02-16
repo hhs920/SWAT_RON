22339f1bd0d4bc19e3bddaa66ae32483689fd6a2
ChoiSW99 <seungwoo714@gmail.com>
2025-02-17 02:54:30 +0900

    Weapon, Projectile 관련 클래스 추가

---
+++ Source/ReadyOrNot/Public/CSW/Weapon/HitScanWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CSW/Weapon/Weapon.h"
+#include "HitScanWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class READYORNOT_API AHitScanWeapon : public AWeapon
+{
+	GENERATED_BODY()
+	
+};
